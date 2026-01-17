fe0081da3aaddd1db916168969b9cbb72be93212
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 21:58:28 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,30 +4,34 @@
 
 int main()
 {
-       char str[99],matris[99][99];
-    int len=0,i=0,j,satir=0,sutun=0;
+    char str[99], matris[99][99];
+    int len = 0, i = 0, j, satir = 0, sutun = 0;
 
     printf("str girin ");
-    scanf("%s",str);
+    scanf("%s", str);
 
-    while(str[i]!='\0'){
-        if(str[i]!='_'){
-            matris[satir][sutun]=str[i];
+    while (str[i] != '\0')
+    {
+        if (str[i] != '_')
+        {
+            matris[satir][sutun] = str[i];
             sutun++;
         }
-        else{
-            matris[satir][sutun]='\0';
+        else
+        {
+            matris[satir][sutun] = '\0';
             satir++;
-            sutun=0;
+            sutun = 0;
         }
         i++;
     }
 
-    matris[satir][sutun]='\0';
+    matris[satir][sutun] = '\0';
 
-    int kelime=satir+1;
+    int kelime = satir + 1;
 
-    for(i=0;i<kelime;i++){
-        printf("%s\n",matris[i]);
+    for (i = 0; i < kelime; i++)
+    {
+        printf("%s\n", matris[i]);
     }
 }
